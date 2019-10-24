ed46e669cd3732b201d1f233af4ff867600822dd
Vaidotas Gružas <34535836+vgruzas@users.noreply.github.com>
2019-10-24 23:07:39 +0300

    1 sprint'as
    1/3 programos

---
+++ psi_2uzduotis/psi_2uzduotis/DB.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace psi_2uzduotis
+{
+    class DB
+    {
+        private IDBOperatable controller;
+        private List<Naudotojai> naud = new List<Naudotojai>();
+
+        public DB(List<Naudotojai> naud)
+        {
+            this.naud = naud;
+        }
+
+        public void Read()
+        {
+            try
+            {
+                string connString = ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+                SqlConnection conn = new SqlConnection(connString);
+                conn.Open();
+                SqlCommand command = conn.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = controller.Read();
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Patikrinkite ar duomenų bazė yra pasiekiama!");
+                Application.Exit();
+            }
+        }
+        public void ReadNaudotojai()
+        {
+            try
+            {
+                string connString = ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+                SqlConnection conn = new SqlConnection(connString);
+                conn.Open();
+                SqlCommand command = conn.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = controller.Read();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Naudotojai n = new Naudotojai(Convert.ToInt32(reader[0]), reader[1].ToString(), reader[2].ToString(), Convert.ToInt32(reader[3]));
+                    naud.Add(n);
+                }
+                conn.Close();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Patikrinkite ar duomenų bazė yra pasiekiama!");
+                Application.Exit();
+            }
+        }
+        public void Update()
+        {
+            try
+            {
+                string connString = ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+                SqlConnection conn = new SqlConnection(connString);
+                conn.Open();
+                SqlCommand command = conn.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = controller.Update();
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Patikrinkite ar duomenų bazė yra pasiekiama!");
+                Application.Exit();
+            }
+        }
+        public void Delete()
+        {
+            try
+            {
+                string connString = ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+                SqlConnection conn = new SqlConnection(connString);
+                conn.Open();
+                SqlCommand command = conn.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = controller.Delete();
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Patikrinkite ar duomenų bazė yra pasiekiama!");
+                Application.Exit();
+            }
+        }
+        public void Insert()
+        {
+            try
+            {
+                string connString = ConfigurationManager.ConnectionStrings["connect"].ConnectionString;
+                SqlConnection conn = new SqlConnection(connString);
+                conn.Open();
+                SqlCommand command = conn.CreateCommand();
+                command.CommandType = System.Data.CommandType.Text;
+                command.CommandText = controller.Insert();
+                command.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (SqlException)
+            {
+                MessageBox.Show("Patikrinkite ar duomenų bazė yra pasiekiama!");
+                Application.Exit();
+            }
+        }
+    }
+}
