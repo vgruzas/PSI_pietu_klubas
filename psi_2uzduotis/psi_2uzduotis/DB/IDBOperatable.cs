5da8f34410b66f68158b24970756cb16090c78da
Vaidotas <vgruzas@gmail.com>
2019-12-11 22:16:00 +0200

    Modified Program

---
+++ psi_2uzduotis/psi_2uzduotis/DB/IDBOperatable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psi_2uzduotis
+{
+    interface IDBOperatable
+    {
+        string Insert();
+        string Update();
+        string Delete();
+        string Read();
+    }
+}
---
+++ psi_2uzduotis/psi_2uzduotis/Models/Naudotojai.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace psi_2uzduotis
+{
+    public class Naudotojai
+    {
+        private int ID;
+        private string vardas;
+        private string slaptazodis;
+        private int type;
+        public Naudotojai(int ID, string vardas, string slaptazodis, int type)
+        {
+            this.ID = ID;
+            this.vardas = vardas;
+            this.slaptazodis = slaptazodis;
+            this.type = type;
+        }
+        public int GetID()
+        {
+            return ID;
+        }
+        public string GetVardas()
+        {
+            return vardas;
+        }
+        public string GetSlap()
+        {
+            return slaptazodis;
+        }
+        public int GetTypeN()
+        {
+            return type;
+        }
+    }
+}
